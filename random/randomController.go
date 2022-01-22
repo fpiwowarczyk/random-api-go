d1ef1983ab604d3d808a197c45ce10ec1a96135f
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2022-01-22 21:55:41 +0100

    Reciving right values, return right structure

--- random/randomController.go
+++ random/randomController.go
@@ -2,23 +2,23 @@ package random
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
+	"sync"
 )
 
 type RandomResponse struct {
 	Stddev float64 `json:"stddev"`
-	Data   []int64 `json:"data"`
-}
-
-type Result struct {
-	FirstNumbers  *[]int `json:"firstNumbers,omitempty"`
-	SecondNumbers *[]int `json:"secondNumbers,omitempty"`
+	Data   []int   `json:"data"`
 }
 
 // Get random numbers and handle wrong input values
 func GetRandomValues(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Call for endpoint: %s", r.URL.String())
 	requestsStr, ok := r.URL.Query()["requests"]
 	requests, err := strconv.Atoi(requestsStr[0])
 	if !ok || err != nil || len(requestsStr) > 1 {
@@ -33,16 +33,60 @@ func GetRandomValues(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	log.Printf("Requests: %d Length: %d", requests, length)
 
-	response := getNumbers(requests, length)
+	randomValues := getNumbers(requests, length)
 
-	json.NewEncoder(w).Encode(response)
+	var responses []RandomResponse
+
+	for _, data := range randomValues {
+		responses = append(responses, RandomResponse{1.0, data})
+	}
+	log.Println(responses)
+	json.NewEncoder(w).Encode(responses)
 }
 
+// Dont call it with more than 5 requests or youll get banned :(
 func getNumbers(req int, len int) [][]int {
-	var responses [][]int
+	var wg sync.WaitGroup
+	var chanArray []chan []int
+	var allValues [][]int
 
-	return responses
+	for i := 0; i < req; i++ {
+		chanArray = append(chanArray, make(chan []int))
+		go func(id int, ch chan []int) {
+			wg.Add(1)
+			log.Printf("Calling goroutine with id:%d \n", id)
+			resp, err := http.Get(fmt.Sprintf("https://www.random.org/integers/?num=%d&min=1&max=100&col=1&base=10&format=plain&rnd=new", len))
+			if err != nil {
+				log.Println(err)
+			}
+			if resp.StatusCode == http.StatusOK {
+				defer resp.Body.Close()
+				bodyBytes, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					log.Println(err)
+				}
+				var values []int
 
+				valuesStr := strings.Split(string(bodyBytes), "\n")
+				for _, i := range valuesStr {
+					if i != "" {
+						value, err := strconv.Atoi(i)
+						if err != nil {
+							log.Println(err)
+						}
+						values = append(values, value)
+					}
+				}
+				ch <- values
+			}
+			wg.Done()
+		}(i, chanArray[i])
+	}
+
+	wg.Wait()
+	for i := 0; i < req; i++ {
+		allValues = append(allValues, <-chanArray[i])
+	}
+	return allValues
 }
